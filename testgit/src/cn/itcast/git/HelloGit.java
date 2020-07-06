53cee7f2d36ae6fc8787b837b531361ded093c77
AllenWoon <AllenWoon@itcast.cn>
2020-07-06 20:19:29 +0800

    新增内容 allen

--- testgit/src/cn/itcast/git/HelloGit.java
+++ testgit/src/cn/itcast/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git,this is my first version");
         System.out.println("version 2");
+        System.out.println("version 3");
     }
 }
