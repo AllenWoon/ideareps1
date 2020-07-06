ec6f91716905db184275ecb4095ea7583c3edabf
AllenWoon <AllenWoon@itcast.cn>
2020-07-06 20:12:01 +0800

    初始化项目 version1

---
+++ testgit/src/cn/itcast/git/HelloGit.java
@@ -0,0 +1,12 @@
+package cn.itcast.git;
+
+/**
+ * @description:
+ * @author: Allen Woon
+ * @time: 2020/7/6 20:04
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git,this is my first version");
+    }
+}
